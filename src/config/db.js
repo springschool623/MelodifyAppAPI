1f58af846eeda195d07b15336fb1f0fa4b059fc0
springschool623 <starxuantruong@gmail.com>
2024-07-22 21:30:53 +0700

    commit88888

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose"
 
 export const connectDB = async (uri) => {
     try {
-        await mongoose.connect(uri)
+        await mongoose.connect(uri, {
+            serverSelectionTimeoutMS: 5000, // Tăng thời gian timeout nếu cần
+            socketTimeoutMS: 45000, // Tăng thời gian socket timeout nếu cần
+            connectTimeoutMS: 10000, // Tăng thời gian connect timeout nếu cần
+        })
     } catch (error) {
         console.log(error)
     }
