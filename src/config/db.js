e41e401b2c071ce11ea23fa66d55185d70ed3081
springschool623 <starxuantruong@gmail.com>
2024-07-22 19:31:55 +0700

    changeName

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async (uri) => {
     try {
-        await mongoose.connect(dbURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            connectTimeoutMS: 30000, // Increase timeout
-            socketTimeoutMS: 45000,  // Increase socket timeout
-        });
+        await mongoose.connect(uri)
     } catch (error) {
         console.log(error)
     }
