611034e409b799cedb542b50ce67e1b4a1fa4c0a
springschool623 <starxuantruong@gmail.com>
2024-07-22 17:43:54 +0700

    commit-444

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import userRouter from './routers/user.js'; // Thêm đuôi .js
-import { connectDB } from './config/db.js';
+import { connectDB } from './config/db';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -17,8 +17,11 @@ connectDB(process.env.DB_URI);
 app.use('/api', userRouter);
 
 const PORT = 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+// Xuất named app
 export const viteNodeApp = app;
+
+export default server; // Xuất mặc định server
