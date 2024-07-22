b3e489215998cba8d04448927d74fc23c7d2609f
springschool623 <starxuantruong@gmail.com>
2024-07-22 18:25:56 +0700

    commit1231332

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,21 +1,12 @@
-import express from "express";
-import { addUser, deleteUser, getUsers, getUserById, updateUser } from "../controller/user.js"; // Thêm đuôi .js
+import express from 'express';
+import { getUsers, getUserById, addUser, deleteUser, updateUser } from '../controllers/user.js';
 
 const router = express.Router();
 
-// Lấy tất cả người dùng
 router.get('/users', getUsers);
-
-// Lấy người dùng theo ID
 router.get('/users/:id', getUserById);
-
-// Thêm người dùng mới
 router.post('/users', addUser);
-
-// Cập nhật người dùng
-router.put('/users/:id', updateUser);
-
-// Xóa người dùng
 router.delete('/users/:id', deleteUser);
+router.put('/users/:id', updateUser);
 
 export default router;
