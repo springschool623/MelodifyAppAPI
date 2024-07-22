83a48cc665dad0dc5b250efa965c44bc3ff3e6e2
springschool623 <starxuantruong@gmail.com>
2024-07-22 19:28:55 +0700

    checkTimeOut

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose"
 
 export const connectDB = async (uri) => {
     try {
-        await mongoose.connect(uri)
+        await mongoose.connect(dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 30000, // Increase timeout
+            socketTimeoutMS: 45000,  // Increase socket timeout
+        });
     } catch (error) {
         console.log(error)
     }
