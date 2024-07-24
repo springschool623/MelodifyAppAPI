3e0db3ff9d6745dc3b635228792518c8e4f0055e
springschool623 <starxuantruong@gmail.com>
2024-07-24 22:38:46 +0700

    commit1221

--- src/model/user.js
+++ src/model/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String, 
         required: true,
-        unique: true
+        unique: false
     },
     password: {
         type: String,
